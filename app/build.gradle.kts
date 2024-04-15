a64505feb12eb2e458c04a914d66bdb58b3ea37e
cagdaspektas <cagdaspektas3405@gmail.com>
2024-04-15 23:51:00 +0300

    services added and linear rv done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,19 +48,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    // Feature module Support
+    implementation ("androidx.navigation:navigation-dynamic-features-fragment:2.7.7")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    //converter-gson
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    //recyclerview
-    implementation ("androidx.recyclerview:recyclerview:1.3.2")
-    // glide
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
     //sdp
     implementation("com.intuit.sdp:sdp-android:1.1.1")
     //ssp
@@ -73,11 +69,16 @@ dependencies {
     //Hilt
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
-
     //Datastore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    //navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    //lottie
+    implementation ("com.airbnb.android:lottie:6.4.0")
+    //converter-gson
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    //recyclerview
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    // glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
 
 }
--- app/src/main/java/com/example/getirapp/presentation/product/ProductFragment.kt
+++ app/src/main/java/com/example/getirapp/presentation/product/ProductFragment.kt
@@ -2,27 +2,105 @@ package com.example.getirapp.presentation.product
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.example.getirapp.R
+import com.example.getirapp.common.domain.ViewState
 import com.example.getirapp.databinding.FragmentProductBinding
+import com.example.getirapp.databinding.ItemProductCardBinding
+import com.example.getirapp.domain.model.BaseResponse
+import com.example.getirapp.domain.model.Product
+import com.example.getirapp.domain.model.ProductItem
+import com.example.getirapp.presentation.adapter.SingleRecylerAdapter
 import com.wada811.viewbindingktx.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class ProductFragment : Fragment(R.layout.fragment_product) {
     private val binding by viewBinding(FragmentProductBinding::bind)
-
-
-
     private val viewModel: ProductViewModel by viewModels()
+
+
+    private val productAdapter = SingleRecylerAdapter<ItemProductCardBinding, ProductItem>(
+        { inflater, _, _ ->
+            ItemProductCardBinding.inflate(
+                inflater,
+                binding.rvProducts,
+                false
+            )
+        },
+        { binding, item, position ->
+            binding.apply {
+                with(item) {
+                    tvPrice.text = priceText.toString()
+                    context?.let {
+                        Glide.with(it)
+                            .load(thumbnailURL)
+                            .into(imgProduct)
+                    }
+                    tvAttribute.text = attribute
+                    tvName.text = name
+                }
+
+
+            }
+        }
+    )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initObserver()
+        viewModel.fetchProduct()
+
+        binding.btnBasket.setOnClickListener {
+            findNavController().navigate(R.id.action_productFragment_to_productFragmentDetail)
+        }
     }
 
+
+    private fun initObserver() = with(viewModel) {
+
+        // fetchSuggestedProducts()
+        viewLifecycleOwner.lifecycleScope.launch {
+            uiStateProduct.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+                .collect { viewState ->
+                    when (viewState) {
+                        is ViewState.Success -> {
+                            val response = viewState.result
+
+                            productAdapter.data = response.data[0].products!!
+
+                            binding.rvProducts.adapter = productAdapter
+
+                        }
+
+                        is ViewState.Error -> {
+                            val responseError = viewState.error
+                            Log.v("MyViewState", responseError)
+                        }
+
+                        is ViewState.Loading -> {
+                            Log.v("MyViewState", "ViewState.Loading")
+                        }
+
+                    }
+
+                }
+        }
+    }
 
 
 }
